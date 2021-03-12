7e85f714d2f187ba9873aa300268f515bb752803
s_labrinos <slabrinos9214@gmail.com>
2021-03-12 17:32:50 +0200

    dependencies updated

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -3,7 +3,7 @@ import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
 import classes from './SideDrawer.module.css';
 import Backdrop from '../../UI/Backdrop/Backdrop';
-import Button from "../../UI/Button/Button";
+// import Button from "../../UI/Button/Button";
 
 /**
  * @author Stavros Lamprinos [stalab at linuxmail.org] on 13/1/2021.
@@ -24,10 +24,10 @@ const sideDrawer = props => {
                 <nav>
                     <NavigationItems/>
                 </nav>
-                <Button btnType="Link"
-                        clicked={() => alert('Log in module here' + props.content)}>
-                    Είσοδος
-                </Button>
+                {/*<Button btnType="Link"*/}
+                {/*        clicked={() => alert('Log in module here' + props.content)}>*/}
+                {/*    Είσοδος*/}
+                {/*</Button>*/}
             </div>
         </React.Fragment>
     );
